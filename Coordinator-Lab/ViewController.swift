f2703b6088ed8ef4fd711b1271e9ccbf6b89610f
thinkySide <eunlyuing@gmail.com>
2024-01-18 14:38:20 +0900

    Initial Commit

---
+++ Coordinator-Lab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coordinator-Lab
+//
+//  Created by 김민준 on 1/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
